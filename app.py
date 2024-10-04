8a21df548c0e961e3c4b2f75142f7e4427ed48cc
jmfdoming <jmfdoming@yahoo.es>
2024-10-04 20:39:04 +0200

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
-# Configuración de la página de Streamlit
+# Configuración de la página de Streamlit para diseño wide
+st.set_page_config(layout="wide")
+
+# Título de la aplicación
 st.title('Visualización de Ticker - Último Año')
 st.write('Ingresa un ticker para ver el gráfico del último año de cotizaciones.')
 
@@ -20,15 +23,16 @@ if ticker:
     if not data.empty:
         # Crear el gráfico
         st.write(f"Gráfico de {ticker} del último año")
-        plt.figure(figsize=(10, 6))
-        plt.plot(data.index, data['Close'], label='Precio de Cierre', color='b')
-        plt.xlabel('Fecha')
-        plt.ylabel('Precio de Cierre (USD)')
-        plt.title(f'Evolución del precio de {ticker} en el último año')
-        plt.legend()
-        plt.grid(True)
-        
-        # Mostrar gráfico en Streamlit
-        st.pyplot(plt)
+        fig, ax = plt.subplots(figsize=(10, 6))  # Mejora en la creación del gráfico
+        ax.plot(data.index, data['Close'], label='Precio de Cierre', color='b')
+        ax.set_xlabel('Fecha')
+        ax.set_ylabel('Precio de Cierre (USD)')
+        ax.set_title(f'Evolución del precio de {ticker} en el último año')
+        ax.legend()
+        ax.grid(True)
+
+        # Mostrar gráfico en Streamlit, adaptado al ancho del contenedor
+        st.pyplot(fig)
     else:
         st.write('No se encontraron datos para el ticker ingresado.')
+
